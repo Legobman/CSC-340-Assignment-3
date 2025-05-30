5933de499d73786f8d971a6d1fa8a4fbf03997a4
Matthew Berwick <msberwick@uncg.edu>
2025-05-29 20:20:50 -0400

    Added Turtle Repository.

---
+++ src/main/java/com/example/demo/turtle/TurtleRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.turtle;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TurtleRepository {
+    @Query(value = "select * from turtles t where t.species like %?1% ", nativeQuery = true)
+    List<Turtle> getTurtleBySpecies(double species);
+
+    @Query(value = "select * from students t where s.name like %?1% ", nativeQuery = true)
+    List<Turtle> getTurtlesByName(String name);
+}
